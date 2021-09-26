c268fdfa2936f6f0090d68c628da527c041784bb
Injustive <xayzenberg.stogniy@gmail.com>
2021-09-26 22:36:30 +0300

    edited loggers

--- loggers/loggers.py
+++ loggers/loggers.py
@@ -1,7 +1,7 @@
 import logging.config
 import yaml
 
-with open('loggers/settings.yml', 'r') as stream:
+with open('loggers/settings.yml', 'r', encoding='unicode_escape') as stream:
     config = yaml.load(stream, Loader=yaml.FullLoader)
 
 logging.config.dictConfig(config)
